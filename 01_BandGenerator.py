1d8208c46e8456556aecc9d8946b1e6577e8f566
joaomendonca-py <jmnm84@gmail.com>
2022-04-02 16:48:08 -0300

    inclusão de 2 projetos já feitos no passado.

---
+++ 01_BandGenerator.py
@@ -0,0 +1,14 @@
+
+#1. Create a greeting for your program.
+print('Bem-vindo ao gerador de nome de banda!')
+#2. Ask the user for the city that they grew up in.
+print('Qual é a sua cidade de nascimento?: ')
+cidade_nasc = input()
+
+
+#3. Ask the user for the name of a pet.
+print('Qual é o nome do seu bicho de estimação?: ')
+pet_name = input()
+
+#4. Combine the name of their city and pet and show them their band name.
+print('O nome da sua banda poderá ser ' + (cidade_nasc), (pet_name) )
